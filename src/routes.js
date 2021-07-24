c0c44a2f458263e8862e3564c557b1c78153dd0f
Andre Oliveira <andreoliveirar2d2@protonmail.com>
2021-07-24 14:47:28 -0300

    feat: importing header and rendering with backgorund

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { ThemeProvider, createTheme, Arwes } from 'arwes';
 
+import Background from './images/background.jpg'
+import Glow from './images/glow.png'
+
+import Header from './components/Header/Header';
 import CreateNote from './pages/CreateNote/CreateNote';
+
+const theme = createTheme({
+  color: {
+    primary: { base: '#00FFFF' }
+  }
+});
 
 const Routes = () => {
   return(
-    <Router>
-      <Route exact path="/" component={CreateNote} />
-    </Router>
+    <ThemeProvider animate theme={theme}>
+      <Arwes animate background={Background} pattern={Glow}>
+        <Header />
+        <Router>
+          <Route exact path="/" component={CreateNote} />
+        </Router>
+      </Arwes>
+    </ThemeProvider>
   );
 }
 
