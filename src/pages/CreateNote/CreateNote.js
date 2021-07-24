e14e516bebd6a6186725097c0584bc54e12a1f00
Andre Oliveira <andreoliveirar2d2@protonmail.com>
2021-07-24 13:40:15 -0300

    feat: routing root to create note page and initializing create route page

---
+++ src/pages/CreateNote/CreateNote.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {} from './CreateNote.styles';
+
+const CreateNote = () => {
+  return(
+    <>
+      <p>Create Note</p>
+    </>
+  );
+}
+
+export default CreateNote;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+
+import CreateNote from './pages/CreateNote/CreateNote';
 
 const Routes = () => {
   return(
-    <>
-    </>
+    <Router>
+      <Route exact path="/" component={CreateNote} />
+    </Router>
   );
 }
 
