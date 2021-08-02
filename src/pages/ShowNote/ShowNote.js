742ebe70d708215476ff929729b6614d3b438443
Andre Oliveira <andreoliveirar2d2@protonmail.com>
2021-08-02 14:03:48 -0300

    feat: opening modal when needs password, and render result when the password is correct

--- src/pages/ShowNote/ShowNote.js
+++ src/pages/ShowNote/ShowNote.js
@@ -6,6 +6,8 @@ import { HiddenInput } from './ShowNote.styles';
 import NotesService from '../../services/notes';
 
 import decrypt from '../../components/utils/decrypt';
+import Modal from '../../components/Modal/Modal';
+import Input from '../../components/Input/Input';
 
 const ShowNote = () => {
   const { slug } = useParams();
@@ -13,6 +15,8 @@ const ShowNote = () => {
   const [title, setTitle] = useState('');
   const [inputFile, setInputFile] = useState('');
   const [privateKey, setPrivateKey] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [password, setPassword] = useState('');
 
   useEffect(() => {
     setInputFile(document.getElementById("input-file"));
@@ -25,9 +29,27 @@ const ShowNote = () => {
   }, [privateKey]);
 
   const loadNote = async() => {
-    const response = await NotesService.show(slug);
+    try {
+      const response = await NotesService.show(slug);
+
+      if (response.status === 200) {
+        setTitle(response.data.title);
+        setBody(response.data.body);
+
+        decrypt(response.data.body, setBody);
+      }
+    } catch {
+      setShowModal(true);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await NotesService.show_with_password(slug, password);
 
     if (response.status === 200) {
+      setShowModal(false);
       setTitle(response.data.title);
       setBody(response.data.body);
 
@@ -69,31 +91,45 @@ const ShowNote = () => {
 
   return(
     <>
-      <Project
-        animate
-        header={title}
-      >
-        {anim => (
-          <p><Words animate show={anim.entered}>
-            { body }
-          </Words></p>
-        )}
-      </Project>
+      { showModal ? (
+        <Modal title='This file is encrypted by password'>
+          <form onSubmit={e => { handleSubmit(e) }}>
+            <label><Words animate>Password</Words></label>
+            <Input onChange={(e) => { setPassword(e.target.value) }} value={password} />
+            <Row s={12} style={{ textAlign: 'center' }}>
+              <Button animate type='submit'>Decrypt</Button>
+            </Row>
+          </form>
+        </Modal>
+      ) : (
+        <>
+          <Project
+            animate
+            header={title}
+          >
+            {anim => (
+              <p><Words animate show={anim.entered}>
+                { body }
+              </Words></p>
+            )}
+          </Project>
 
-      <Row col s={6} m={4} offset={['s3', 'm4']} style={{ textAlign: 'center' }}>
-        <HiddenInput type="file" id="input-file" onChange={e => addPrivateKey(e)} />
-        <textarea type="text" id="input-public-key" value={privateKey} onChange={() => {}} style={{opacity: 0}} />
+          <Row col s={6} m={4} offset={['s3', 'm4']} style={{ textAlign: 'center' }}>
+            <HiddenInput type="file" id="input-file" onChange={e => addPrivateKey(e)} />
+            <textarea type="text" id="input-public-key" value={privateKey} onChange={() => {}} style={{opacity: 0}} />
 
-        <br />
-        { privateKey === '' || privateKey === null ? (
-          <Button animate onClick={() => handleUpload()}>Upload Private Key</Button>
-        ) : (
-          <>
-            <Button animate onClick={e => handleCopy()} style={{ marginRight: 20 }}>Export Private Key to Clipboard</Button>
-            <Button animate layer='alert' onClick={() => cleanKey() }>Delete Private Key</Button>
-          </>
-        ) } 
-      </Row>
+            <br />
+            { privateKey === '' || privateKey === null ? (
+              <Button animate onClick={() => handleUpload()}>Upload Private Key</Button>
+            ) : (
+              <>
+                <Button animate onClick={e => handleCopy()} style={{ marginRight: 20 }}>Export Private Key to Clipboard</Button>
+                <Button animate layer='alert' onClick={() => cleanKey() }>Delete Private Key</Button>
+              </>
+            ) } 
+          </Row>
+        </>
+      ) }
     </>
   );
 }
